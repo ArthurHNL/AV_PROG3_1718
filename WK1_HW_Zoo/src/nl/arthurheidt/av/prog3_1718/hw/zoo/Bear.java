89bc5159e39985b94bd3bd84002903c5d4505fcc
Arthur Heidt <arthur@arthurheidt.nl>
2018-02-01 13:51:51 +0100

    changed some stuff

--- WK1_HW_Zoo/src/nl/arthurheidt/av/prog3_1718/hw/zoo/Bear.java
+++ WK1_HW_Zoo/src/nl/arthurheidt/av/prog3_1718/hw/zoo/Bear.java
@@ -2,17 +2,21 @@ package nl.arthurheidt.av.prog3_1718.hw.zoo;
 
 /**
  * A bear to put in a zoo.
+ * 
  * @author Arthur Heidt
  *
  */
 public class Bear extends Animal {
-	
-	/**
-	 * Create a new bear
-	 * @param name name of the frog.
-	 * @throws IllegalArgumentException if the name is empty
-	 */
-	public Bear(String name) throws IllegalArgumentException {
-		super(name, "ROAR!");
-	}
+
+    /**
+     * Create a new bear
+     * 
+     * @param name
+     *            name of the frog.
+     * @throws IllegalArgumentException
+     *             if the name is empty
+     */
+    public Bear(String name) throws IllegalArgumentException {
+	super(name, "ROAR!");
+    }
 }
