57ae03388dd6b21d79a4a81af892305a452d5e10
Arthur Heidt <arthur@arthurheidt.nl>
2018-02-08 15:01:48 +0100

    Added pr2b

--- WK2_PRB_Airport/src/nl/arthurheidt/av/prog3/airportv2/Airplane.java
+++ WK2_PRB_Airport/src/nl/arthurheidt/av/prog3/airportv2/Airplane.java
@@ -1,24 +1,15 @@
 package nl.arthurheidt.av.prog3.airportv2;
 
-public abstract class Airplane implements Trackable {
+public abstract class Airplane {
     // NO error cause it is valid code.
     // A constructor in an abstact class can be useful to call from a constructor
     // from an inheriting non-abstract class to set private instance variables.
 
-    private int xPos;
-    private int yPos;
-    private int zPos;
 
-    public Airplane(int xPos, int yPos, int altitude) {
-	this.xPos = xPos;
-	this.yPos = yPos;
-	this.zPos = altitude;
+    public Airplane() {
+
     }
 
-    public void showInfoOnRadar() {
-	System.out.println(
-		"[RADAR] X: " + xPos + " Y: " + yPos + " ALT: " + zPos + " " + this.getClass().getSimpleName());
-    }
 
     // NO error cause it is valid code.
     // A method in an abstrac class can be userfull because it is inherited by
--- WK2_PRB_Airport/src/nl/arthurheidt/av/prog3/airportv2/Airport.java
+++ WK2_PRB_Airport/src/nl/arthurheidt/av/prog3/airportv2/Airport.java
@@ -4,13 +4,30 @@ import java.util.ArrayList;
 
 public class Airport {
     ArrayList<Airplane> planes;
+    Radar rad;
 
     public Airport() {
 	planes = new ArrayList<Airplane>();
+	rad = new Radar();
     }
 
     public void addPlane(Airplane p) {
+	if (p == null) {
+	    throw new IllegalArgumentException("No null planes!");
+	}
+	if (p instanceof Trackable) {
+	    //Need to cast
+	    rad.addTrackable((Trackable) (p));
+	}
 	planes.add(p);
+    }
+
+    public void addPlanes(Airplane... p) {
+	for (Airplane ap : p) {
+	    if (ap != null) {
+		this.addPlane(ap);
+	    }
+	}
     }
 
     public Airplane getPlane(int index) {
@@ -22,8 +39,10 @@ public class Airport {
     }
 
     public void showAllContents() {
-	for (Airplane p : planes)
+	rad.showTrackables();
+	for (Airplane p : planes) {
 	    showContents(p);
+	}
     }
 
     private void showContents(Airplane p) {
@@ -39,6 +58,10 @@ public class Airport {
 	if (p instanceof TransportPlane) {
 	    TransportPlane tp = (TransportPlane) p;
 	    tp.showPassengerAmount();
+	    return;
+	}
+	if (p instanceof StealthPlane) {
+	    System.out.println("Classified millitary apparatus.");
 	    return;
 	}
 	System.out.println("Unknown plane type detected!");
