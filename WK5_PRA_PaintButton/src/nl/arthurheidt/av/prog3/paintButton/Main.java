d18dd57ff9934e321ca945c8cc7093bb5317abf9
Arthur Heidt <arthur@arthurheidt.nl>
2018-03-06 21:24:12 +0100

    added wk5 pra

---
+++ WK5_PRA_PaintButton/src/nl/arthurheidt/av/prog3/paintButton/Main.java
@@ -0,0 +1,13 @@
+package nl.arthurheidt.av.prog3.paintButton;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyGUI MyGUI = new MyGUI("Paint Demo", new PaintDemoPane());
+		MyGUI.packAndShow();
+
+	}
+
+}
